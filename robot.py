2baf71f5e9aa2bdadd7c49b5f4d5bcea88142431
Anuraag Bhattacharya <anubhatt50@gmail.com>
2021-03-28 15:11:55 +0530

    update

--- robot.py
+++ robot.py
@@ -1,36 +1,41 @@
 from utils import shortest_path
 
 class Robot:
-	def __init__(self, name, optimal_paths, tasks_left, graph):
+    def __init__(self, name, optimal_paths, tasks_left, graph):
 
-		self.name = name
-		self.optimal_paths = optimal_paths
-		self.tasks_left = tasks_left
-		self.graph = graph
-		self.task_idx = 0
-		self.loc_idx = 0
+        self.name = name
+        self.optimal_paths = optimal_paths
+        self.tasks_left = tasks_left
+        self.graph = graph
+        self.task_idx = 0
+        self.loc_idx = 0
+        self.task_part_idx = 0
+        self.goal_idx = [self.optimal_paths[0][0][-1],self.optimal_paths[0][1][-1],self.optimal_paths[0][2][-1]]
 
-		self.pos = (self.optimal_paths[0][0][0], self.optimal_paths[0][0][1])
+        self.pos = (self.optimal_paths[0][0][0][0], self.optimal_paths[0][0][0][1])
 
-		if len(self.optimal_paths)>1:
-			self.add_path_to_new_tasks(graph)
+        if len(self.optimal_paths)>1:
+            self.add_path_to_new_tasks(graph)
 
-	def add_path_to_new_tasks(graph):
-		idx = 0
-		while idx+1<len(self.optimal_paths):
-			if self.optimal_paths[idx][-1] != self.optimal_paths[idx+1][0]:
-				path = shortest_path(graph, self.optimal_paths[idx][-1], self.optimal_paths[idx+1][0])
-				self.optimal_paths.insert(idx+1, path)
-				idx += 2
-			else:
-				idx += 1
+    def add_path_to_new_tasks(graph):
+        idx = 0
+        while idx+1<len(self.optimal_paths):
+            if self.optimal_paths[idx][-1][-1] != self.optimal_paths[idx+1][0][0]:
+                path = shortest_path(graph, self.optimal_paths[idx][-1][-1], self.optimal_paths[idx+1][0][0])
+                self.optimal_paths.insert(idx+1, path)
+                idx += 2
+            else:
+                idx += 1
 
-	def reroute():
-		'''
-		Reroute path if collision between multiple robots
-		at task->self.task_idx and loc->self.loc_idx by ignoring
-		collison block.
-		(Take a step back, block current cell as other robot occupies it, 
-		and find shortest path in the given scenario)
-		'''
-		pass
+    def reroute(new_loc):
+        '''
+        Reroute path if collision between multiple robots
+        at task->self.task_idx and loc->self.loc_idx by ignoring
+        collison block.
+        (Take a step back, block current cell as other robot occupies it, 
+        and find shortest path in the given scenario)
+        '''
+        
+        new_path = shortest_path(graph, self.optimal_paths[self.task_idx][self.task_part_idx][self.loc_idx], self.goal_idx[self.task_part_idx], addn_blocked = new_loc)
+        self.optimal_paths[self.task_idx][self.task_part_idx] = self.optimal_paths[self.task_idx][self.task_part_idx][0:self.loc_idx] + new_path
+
--- utils.py
+++ utils.py
@@ -2,113 +2,115 @@ import json
 import numpy as np
 
 def create_graph(json_path):
-	'''
-	Return graph in the form of a dictionary
-	'''
-	graph = {}
+    '''
+    Return graph in the form of a dictionary
+    '''
+    graph = {}
 
-	f = open(json_path, 'r')
-	data = json.load(f)
+    f = open(json_path, 'r')
+    data = json.load(f)
 
-	graph['n'] = data['n']
-	graph['m'] = data['m']
-	
-	graph['blocked'] = set()
-	for indices in data['blocked']:
-		graph['blocked'].add(tuple(indices))
-	
-	graph['TS'] = set()
-	for indices in data['TS']:
-		graph['TS'].add(tuple(indices))
-	
-	f.close()
+    graph['n'] = data['n']
+    graph['m'] = data['m']
 
-	return graph
+    graph['blocked'] = set()
+    for indices in data['blocked']:
+        graph['blocked'].add(tuple(indices))
+
+    graph['TS'] = set()
+    for indices in data['TS']:
+        graph['TS'].add(tuple(indices))
+
+    f.close()
+
+    return graph
 
 def read_task(txt_path):
-	'''
-	Return dict of tasks
-	'''
-	f = open(txt_path, 'r')
-	data = f.read().split()
+    '''
+    Return dict of tasks
+    '''
+    f = open(txt_path, 'r')
+    data = f.read().split()
 
-	tasks = {}
-	for line in data:
-		line = line.split(',')
-		if line[0] not in tasks:
-			tasks[line[0]] = []
-		init = tuple([int(line[1]), int(line[2])])
-		final = tuple([int(line[3]), int(line[4])])
-		pickup = tuple([int(line[5]), int(line[6])])
-		delivery = tuple([int(line[7]), int(line[8])])
-		tasks[line[0]].append([init, final, pickup, delivery])
+    tasks = {}
+    for line in data:
+        line = line.split(',')
+        if line[0] not in tasks:
+            tasks[line[0]] = []
+        init = tuple([int(line[1]), int(line[2])])
+        final = tuple([int(line[3]), int(line[4])])
+        pickup = tuple([int(line[5]), int(line[6])])
+        delivery = tuple([int(line[7]), int(line[8])])
+        tasks[line[0]].append([init, final, pickup, delivery])
 
-	return tasks
+    return tasks
 
-def shortest_path(graph, start, goal):
-	n, m = graph['n'], graph['m']
+def shortest_path(graph, start, goal, addn_blocked = None):
+    n, m = graph['n'], graph['m']
 
-	# Set of visited points
-	vis = set()
+    # Set of visited points
+    vis = set()
+    if addn_blocked != None: vis.add(addn_blocked)
 
-	# dict to store parents
-	par = {}
+    # dict to store parents
+    par = {}
 
-	# Create queue for BFS
-	queue = []
+    # Create queue for BFS
+    queue = []
 
-	queue.append(start)
+    queue.append(start)
 
-	while queue:
-		s = queue.pop(0)
-		
-		# If s has been visited before, continue
-		if s in vis or s in graph['blocked']:
-			continue
-		
-		# If goal reached, return path	
-		if s[0]==goal[0] and s[1]==goal[1]:
-			path = [goal]
-			while s != start:
-				path.append(par[s])
-				s = par[s]
-			path.reverse()
-			return path
+    while queue:
+        s = queue.pop(0)
 
-		# Add s to visited set
-		vis.add(s)
+        # If s has been visited before, continue
+        if s in vis or s in graph['blocked']:
+            continue
 
-		# Visit neighbours of s
-		if (s[0]+1)<n and (s[0]+1)>=0:
-			queue.append((s[0]+1, s[1]))
-			if (s[0]+1, s[1]) not in par:
-				par[(s[0]+1, s[1])] = s
-		if (s[0]-1)<n and (s[0]-1)>=0:
-			queue.append((s[0]-1, s[1]))
-			if (s[0]-1, s[1]) not in par:
-				par[(s[0]-1, s[1])] = s
-		if (s[1]+1)<m and (s[1]+1)>=0:
-			queue.append((s[0], s[1]+1))
-			if (s[0], s[1]+1) not in par:
-				par[(s[0], s[1]+1)] = s
-		if (s[1]-1)<m and (s[1]-1)>=0:
-			queue.append((s[0], s[1]-1))
-			if (s[0], s[1]-1) not in par:
-				par[(s[0], s[1]-1)] = s
+        # If goal reached, return path	
+        if s[0]==goal[0] and s[1]==goal[1]:
+            path = [goal]
+            while s != start:
+                path.append(par[s])
+                s = par[s]
+            path.reverse()
+            return path
+
+        # Add s to visited set
+        vis.add(s)
+
+        # Visit neighbours of s
+        if (s[0]+1)<n and (s[0]+1)>=0:
+            queue.append((s[0]+1, s[1]))
+            if (s[0]+1, s[1]) not in par:
+                par[(s[0]+1, s[1])] = s
+        if (s[0]-1)<n and (s[0]-1)>=0:
+            queue.append((s[0]-1, s[1]))
+            if (s[0]-1, s[1]) not in par:
+                par[(s[0]-1, s[1])] = s
+        if (s[1]+1)<m and (s[1]+1)>=0:
+            queue.append((s[0], s[1]+1))
+            if (s[0], s[1]+1) not in par:
+                par[(s[0], s[1]+1)] = s
+        if (s[1]-1)<m and (s[1]-1)>=0:
+            queue.append((s[0], s[1]-1))
+            if (s[0], s[1]-1) not in par:
+                par[(s[0], s[1]-1)] = s
+
 
 def tasks_remaining(robots):
-	'''
-	Returning if any task remaining or not
+    '''
+    Returning if any task remaining or not
 
-	Input : 
-		list of robot objects
-	
-	Output : 
-		True -> Task remaining
-		False -> Tasks over
-	'''
+    Input : 
+        list of robot objects
 
-	for robot in robots:
-		if robot.task_idx < len(robot.optimal_paths):
-			return True
-	return False
+    Output : 
+        True -> Task remaining
+        False -> Tasks over
+    '''
+
+    for robot in robots:
+        if robot.task_idx < len(robot.optimal_paths):
+            return True
+    return False
